3fb8f68f4733db2b43e1a362022ab8597cec66ff
antman666 <62091572+antman666@users.noreply.github.com>
2024-12-12 10:44:54 +0800

    Update aria2-gnu-linux-build-amd64.sh

--- aria2-gnu-linux-build-amd64.sh
+++ aria2-gnu-linux-build-amd64.sh
@@ -29,12 +29,12 @@ export CURL_CA_BUNDLE="/etc/ssl/certs/ca-certificates.crt"
 export PKG_CONFIG_PATH="$PREFIX/lib/pkgconfig"
 export LD_LIBRARY_PATH="$PREFIX/lib"
 export CFLAGS+="-march=x86-64-v3"
-export CC="clang"
-export CXX="clang++"
-export STRIP="llvm-strip"
-export RANLIB="llvm-ranlib"
-export AR="llvm-ar"
-export LD="lld"
+export CC="clang-19"
+export CXX="clang++-19"
+export STRIP="llvm-strip-19"
+export RANLIB="llvm-ranlib-19"
+export AR="llvm-ar-19"
+export LD="lld-19"
 
 ## DEPENDENCES ##
 source $SCRIPT_DIR/dependences
